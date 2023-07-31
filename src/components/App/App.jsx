1e69d8d873101d7c1114b1b81011f3d77b787de4
AnastasiaMoiseeva0 <Shinaeva1997@mail.ru>
2023-07-31 17:51:14 +0300

    feat: добавлен перечень компонентов, логотип в Header

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import Header from "../Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
