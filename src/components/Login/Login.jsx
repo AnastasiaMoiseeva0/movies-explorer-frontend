9408b18e67cc89d77f6177c982740de5828ec398
AnastasiaMoiseeva0 <Shinaeva1997@mail.ru>
2023-08-16 18:19:34 +0300

    feat: добавлена аутентификация пользователя

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,35 @@
 import "./Login.css";
 import Button from "../Button/Button";
 import Logo from "../Logo/Logo";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Input from "../Input/Input";
+import * as auth from "../../utils/auth";
+import { useForm } from "../../hooks/useForm.js";
 
-function Login() {
+function Login({handleLogin}) {
+ const {values, handleChange, setValues} = useForm({});
+ const navigate = useNavigate();
+
+ function onLogin(event) {
+  event.preventDefault();
+  if (!values.email || !values.password) {
+    return;
+  }
+  auth
+    .authorize(values.password, values.email)
+    .then((data) => {
+      if (data.token) {
+        setValues({ password: "", email: "" });
+        handleLogin();
+        navigate("/movies", { replace: true });
+      }
+    })
+    .catch((err) => console.log(err));
+}
+
   return (
     <main className="register">
-      <form className="register__form">
+      <form className="register__form" onSubmit={onLogin}>
         <Logo className="register__logo" />
         <h2 className="register__title">Рады видеть!</h2>
         <Input
@@ -15,12 +37,16 @@ function Login() {
           type="email"
           name="email"
           isValidate={false}
+          value={values.email || ''}
+          onChange={handleChange}
         />
         <Input
           text="Пароль"
           type="password"
           name="password"
           isValidate={false}
+          value={values.password || ''}
+          onChange={handleChange}
         />
         <Button
           className="register__button register__button_type_signin"
