13737686d538f346a8fb163a382e42e378588f63
AnastasiaMoiseeva0 <Shinaeva1997@mail.ru>
2023-08-25 15:47:57 +0300

    fix: авторизация удалена из moviesApi

--- src/utils/moviesApi.js
+++ src/utils/moviesApi.js
@@ -18,7 +18,6 @@ class MoviesApi {
 const moviesApi = new MoviesApi({
   baseUrl: "https://api.nomoreparties.co/beatfilm-movies",
   headers: {
-    authorization: "110366df-b38b-45be-88c4-715aacdd3349",
     "Content-Type": "application/json",
   },
 });
